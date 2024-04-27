1c8a9e00aff3ab2c4a06ecc6989322783615aae5
Eduardo Kropniczki <dudukrop@gmail.com>
2024-04-27 11:38:46 -0300

    first commit

---
+++ Exercicio - Herança/Entities/Employee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio___Herança.Entities
+{
+    class Employee
+    {
+
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHour { get; set; }
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+
+        public Employee()
+        {
+
+        }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+        public virtual double Payment()
+        {
+            return ValuePerHour * Hours;
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            Employees.Add(employee);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (Employee employee in Employees)
+            {
+                sb.AppendLine(employee.Name + " - $ " + employee.Payment().ToString("F2", CultureInfo.InvariantCulture));
+            }
+            
+            return sb.ToString();
+        }
+    }
+}
---
+++ Exercicio - Herança/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Exercicio___Herança.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio___Herança
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee emp = new Employee();
+
+            Console.Write("Enter the number of employees: ");
+            int numberOfEmployees = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 1; i <= numberOfEmployees; i++)
+            {
+                Console.WriteLine("Employee #" + i + " data:");
+                Console.Write("Outsourced (y/n)? ");
+                char yN = char.Parse(Console.ReadLine());
+
+                if (yN == 'n')
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Hours: ");
+                    int hours = int.Parse(Console.ReadLine());
+                    Console.Write("Value per hour: ");
+                    double valuePerHour = double.Parse(Console.ReadLine());
+
+                    Employee employee = new Employee(name, hours, valuePerHour);
+                    emp.AddEmployee(employee);
+                    Console.WriteLine();
+                }
+
+                else
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Hours: ");
+                    int hours = int.Parse(Console.ReadLine());
+                    Console.Write("Value per hour: ");
+                    double valuePerHour = double.Parse(Console.ReadLine());
+                    Console.Write("Additional charge: ");
+                    double charge = double.Parse(Console.ReadLine());
+
+                    OutSourcedEmployee outSourcedEmployee = new OutSourcedEmployee(name, hours, valuePerHour, charge);
+                    emp.AddEmployee(outSourcedEmployee);
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine(emp);
+            Console.ReadLine();
+        }
+    }
+}
